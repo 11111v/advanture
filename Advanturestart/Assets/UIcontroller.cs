e0bf68880dc00258a7cb6350054742ade8b03099
nan <z17649862647@gmail.com>
2023-04-01 13:22:09 +0800

    初始化第一次提交

---
+++ Advanturestart/Assets/UIcontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIcontroller : MonoBehaviour
+{
+    private Button StartButton;
+    public GameObject StartPanel;
+    private void Start()
+    {
+        StartButton = transform.Find("Canvas/startPanel/startButton").GetComponent<Button>();
+        //StartPanel = GameObject.Find("Canvas/startPanel");
+        StartButton.onClick.AddListener(startGame);
+    }
+    private void startGame()
+    {
+        StartPanel.SetActive(false);
+    }
+    private void Update()
+    {
+
+    }
+}
